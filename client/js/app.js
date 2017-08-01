0b71a30d5d10916b60fec0cc6efe51652cd6d24c
Liam Hurwitz <liam@contra-bit.com>
2017-08-01 21:27:50 +0200

    ready for drones

--- client/js/app.js
+++ client/js/app.js
@@ -1,33 +1,33 @@
-function getSessionList(success, error) {
-  var soql = "SELECT Session__r.Id, Session__r.Name FROM Session_Speaker__c";
+function getFLugList(success, error) {
+  var soql = "SELECT Flug__r.Id, Flug__r.Name FROM Flug_Pilot__c";
   force.query(soql, success, error);
 }
 
-function getSessionDetails(sessionId, success, error) {
-  var soql = "SELECT Session__r.Name, " +
-  "Session__r.Session_Date__c, " +
-  "Speaker__r.First_Name__c, " +
-  "Speaker__r.Last_Name__c " +
-  "FROM Session_Speaker__c " +
-  "WHERE Session__r.Id = '" + sessionId + "'";
+function getFlugDetails(flugId, success, error) {
+  var soql = "SELECT Flug__r.Name, " +
+  "Flug__r.Session_Date__c, " +
+  "Pilot__r.Vorname__c, " +
+  "Pilot__r.Nachname__c " +
+  "FROM Flug_Pilot__c " +
+  "WHERE Flug__r.Id = '" + flugId + "'";
   force.query(soql, success, error);
 }
 
-function showSessionList() {
-    getSessionList(
+function showFlugList() {
+    getFLugList(
         function (data) {
-            var sessions = data.records,
+            var flugs = data.records,
                 html = '';
-            for (var i=0; i<sessions.length; i++) {
-                html += '<li class="table-view-cell"><a href="#sessions/'+ sessions[i].Session__r.Id +'">' + sessions[i].Session__r.Name + '</a></li>';
+            for (var i=0; i<flugs.length; i++) {
+                html += '<li class="table-view-cell"><a href="#flugs/'+ flugs[i].Flug__r.Id +'">' + flugs[i].Flug__r.Name + '</a></li>';
             }
             html =
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
-                    '<h1 class="title">Sessions</h1>' +
+                    '<h1 class="title">Flugs</h1>' +
                 '</header>' +
                 '<div class="content">' +
-                    '<ul class="table-view session-list">' + html + '</ul>' +
+                    '<ul class="table-view flug-list">' + html + '</ul>' +
                 '</div>' +
                 '</div>';
             slider.slidePage($(html));
@@ -38,29 +38,29 @@ function showSessionList() {
     return false;
 }
 
-function showSessionDetails(sessionId) {
+function showFlugDetails(flugId) {
 
-    getSessionDetails(sessionId,
+    getFlugDetails(flugId,
         function (data) {
-            var session = data.records[0],
+            var flug = data.records[0],
             html =
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
                 '<a class="btn btn-link btn-nav pull-left" href="#"><span class="icon icon-left-nav"></span>Back</a>' +
-            '<h1 class="title">Sessions</h1>' +
+            '<h1 class="title">Flugs</h1>' +
                 '</header>' +
                 '<div class="content">' +
                     '<div class="card">' +
                         '<ul class="table-view">' +
                             '<li class="table-view-cell">' +
-                                '<h4>' + session.Session__r.Name + '</h4>' +
-                                '<p>' + (session.Session__r.Session_Date__c || 'No time yet')+ '</p>' +
+                                '<h4>' + flug.Flug__r.Name + '</h4>' +
+                                '<p>' + (flug.Flug__r.Flug_Date__c || 'No time yet')+ '</p>' +
                             '</li>' +
-                            '<li class="table-view-cell">Speaker: ' +
-                                session.Speaker__r.First_Name__c +
+                            '<li class="table-view-cell">Pilot: ' +
+                                flug.Pilot__r.Vorname__c +
                             '</li>' +
                             '<li class="table-view-cell">' +
-                                (session.Session__r.Description__c || 'No description yet') +
+                                (flug.Flug__r.Beschreibung__c || 'No description yet') +
                             '</li>' +
                         '</ul>' +
                     '</div>' +
@@ -75,5 +75,5 @@ function showSessionDetails(sessionId) {
 }
 
 var slider = new PageSlider($('body')); // Initialize PageSlider micro-library for nice and hardware-accelerated page transitions
-router.addRoute('', showSessionList);
-router.addRoute('sessions/:id', showSessionDetails);
+router.addRoute('', showFlugList);
+router.addRoute('flugs/:id', showFlugDetails);
